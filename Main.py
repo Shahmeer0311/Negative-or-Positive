c2fd7f04eb417434d3e8c963f2a84b5bd3b6e4a8
Shahmeer0311 <22alis@students.watfordboys.org>
2024-12-01 14:19:28 +0000

    Negative or Positive

---
+++ Main.py
@@ -0,0 +1,7 @@
+Number = int(input("Enter Number"))
+if (Number > 0):
+    print("Your Number is Positive")
+elif (Number == 0):
+    print("Your Number is 0")
+else:
+    print("Your Number is Negative")
